972c599e5a05672a47550cbeb838cec2342c8890
Praveen1221-TF <praveensunnam123@gmail.com>
2025-10-27 12:24:53 +0530

    terraform-sg

---
+++ terraform-aws-vpc/variables.tf
@@ -0,0 +1,93 @@
+variable "vpc_cidr" {
+    type = string
+    description = "VPC cidr"
+}
+
+variable "project_name" {
+   type = string   
+}
+
+variable "environment" {
+    type = string
+}
+variable "vpc_tags" {
+    type=map
+    default = {}
+  
+}
+variable "igw_tags" {
+    type = map
+    default = {}
+  
+}
+
+variable "public_subnet_cidr" {
+    type = list
+  
+}
+
+variable "public_subnet_tags" {
+    type= map
+    default = {}
+  
+}
+
+variable "private_subnet_cidr" {
+    type = list
+  
+}
+
+variable "private_subnet_tags" {
+    type= map
+    default = {}
+  
+}
+
+variable "database_subnet_cidr" {
+    type = list
+  
+}
+
+variable "database_subnet_tags" {
+    type= map
+    default = {}
+  
+}
+
+variable "public_route_table_tags" {
+  type = map
+  default = {}
+}
+
+variable "private_route_table_tags" {
+  type = map
+  default = {}
+}
+
+variable "database_route_table_tags" {
+  type = map
+  default = {}
+}
+
+variable "eip_tags" {
+    type = map
+    default = {}
+
+  
+}
+
+variable "nat_gateway_tags" {
+
+    type = map 
+    default = {}
+  
+}
+
+variable "is_peering_requried" {
+    type = bool
+    default = true
+  
+}
+
+
+
